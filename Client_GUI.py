c9465a69fe80c9c7a96af56661f518a95360cbd7
VisedFeline121 <30751326+VisedFeline121@users.noreply.github.com>
2018-03-07 00:57:40 +0200

    Update Client_GUI.py

--- Client_GUI.py
+++ Client_GUI.py
@@ -3,6 +3,7 @@ import Tkinter as tk
 import ScrolledText
 import logging
 from ttk import Treeview
+from init_client import Client
 
 
 class TextHandler(logging.Handler):
@@ -22,7 +23,7 @@ class TextHandler(logging.Handler):
         self.text.after(0, append)
 
 
-class Client(tk.Frame):
+class Client_GUI(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master = master
@@ -50,10 +51,16 @@ class Client(tk.Frame):
         self.port_label = None
         self.connect_button = None
 
+        self.host_var = tk.StringVar(self.master, value='localhost')
+        self.user_var = tk.StringVar(self.master, value='ido')
+        self.password_var = tk.StringVar(self.master, value='1234')
+        self.port_var = tk.StringVar(self.master, value='21')
+        self.command_var = tk.StringVar(self.master, value='retr sup.txt sup.txt')
+
         self.master.option_add("*tearOff", 'False')
 
         self.st = ScrolledText.ScrolledText(self.master, state='disabled')
-        self.st.configure(font=('TkFixedFont', 16), height=7)
+        self.st.configure(font=('TkFixedFont', 10), height=7)
         self.st.grid(column=0, row=1, sticky='nwe', columnspan=9)
 
         self.text_handler = TextHandler(self.st)
@@ -64,6 +71,8 @@ class Client(tk.Frame):
 
         self.logger = logging.getLogger()
         self.logger.addHandler(self.text_handler)
+
+        self.client = Client()
 
         self.create_menu_bar()
         self.create_connection_widgets()
@@ -89,18 +98,19 @@ class Client(tk.Frame):
 
     def create_connection_widgets(self):
         self.host_label = tk.Label(self.master, text="Host:").grid(row=0, column=0)
-        self.host_entry = tk.Entry(self.master).grid(row=0, column=1)
+        self.host_entry = tk.Entry(self.master, textvariable=self.host_var).grid(row=0, column=1)
 
         self.user_label = tk.Label(self.master, text="Username:").grid(row=0, column=2)
-        self.user_entry = tk.Entry(self.master).grid(row=0, column=3)
+        self.user_entry = tk.Entry(self.master, textvariable=self.user_var).grid(row=0, column=3)
 
         self.password_label = tk.Label(self.master, text="Password:").grid(row=0, column=4)
-        self.password_entry = tk.Entry(self.master, show="*").grid(row=0, column=5)
+        self.password_entry = tk.Entry(self.master, textvariable=self.password_var, show="*").grid(row=0, column=5)
 
         self.port_label = tk.Label(self.master, text="Port:").grid(row=0, column=6)
-        self.port_entry = tk.Entry(self.master).grid(row=0, column=7)
+        self.port_entry = tk.Entry(self.master, textvariable=self.port_var).grid(row=0, column=7)
 
-        self.connect_button = tk.Button(self.master, text="Connect", command=None).grid(row=0, column=8)
+        self.connect_button = tk.Button(self.master, text="Connect",
+                                        command=lambda: self.init_socket_connection()).grid(row=0, column=8)
 
     def create_user_tree(self):
         self.user_tree = Treeview(self.master)
@@ -115,8 +125,8 @@ class Client(tk.Frame):
     def create_arbitrary_command(self):
         self.command_label = tk.Label(self.master,
                                       text="Send Arbitrary Command:").grid(row=3, column=0)
-        self.command_entry = tk.Entry(self.master).grid(row=3, column=1)
-        self.command_button = tk.Button(self.master, text="Send Command", command=None).grid(row=3, column=2)
+        self.command_entry = tk.Entry(self.master, textvariable=self.command_var).grid(row=3, column=1)
+        self.command_button = tk.Button(self.master, text="Send Command", command=self.add_to_log).grid(row=3, column=2)
 
     def insert_dirs_to_tree(self, tree, parent_id, dir_id, content):
         for i in content:
@@ -126,15 +136,43 @@ class Client(tk.Frame):
         for i in content:
             tree.insert(parent_id, 'end', text=i)
 
+    def init_socket_connection(self):
+        if self.port_var.get() != '':
+            self.client.connect(self.host_var.get(), self.port_var.get())
+        else:
+            self.client.connect(self.host_var.get())
+        #logging.info(self.client.OPTS(['UTF8', 'ON']))
+        #logging.info(self.client.get_response())
+        logging.info(self.client.USER([self.user_var.get()]))
+        logging.info(self.client.PASS([self.password_var.get()]))
+        logging.info(self.client.CWD(['/']))
+
+    def add_to_log(self):
+        logging.info(self.send_command())
+
+    def send_command(self):
+        raw_command = self.command_var.get()
+        raw_command = raw_command.split(' ')
+        command = raw_command[0]
+        command = command.upper()
+        command = self.client.command_dict[command]
+        if len(raw_command) > 1:
+            params = raw_command[1:]
+            return command(params)
+        else:
+            return command()
+
     def run(self):
         self.master.mainloop()
 
 
 def main():
     master = tk.Tk()
-    app = Client(master)
+    app = Client_GUI(master)
     app.run()
 
 
 if __name__ == '__main__':
     main()
+
+#192.168.3.22
